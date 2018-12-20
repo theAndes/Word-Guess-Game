13ef62472dfffc329bbb7ca7ee065b1c21b8d645
AGE <age.esquivel@gmail.com>
2018-12-19 21:33:01 -0600

    starting the JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,70 @@
 
-
-
-//list of words for game//
-let wordList = ['word','guess','game','web','Austin','debug']
-
-
 //random word//
-let word = wordList[Math.floor(Math.random() * wordList.length)];
+var wordList = ['word', 'guess', 'game', 'web', 'austin', 'debug', 'development', 'full', 'stack']
+var word = wordList[Math.floor(Math.random() * wordList.length)];
+var hiddenWord = [];
 console.log(word);
+var tries = word.length + wordList.length;
+var endTries = 0;
+var guessedLetters = [];
+var win = 0;
+var lose = 0;
 
 
-//split the random word characters into a list//
-let wordItems = word.split('')
-console.log(wordItems);
+//create hidden word
 
+function startGame() {
+    for (let i = 0; i < word.length; i++) { //create the '_' in html
+        hiddenWord[i] = "__";
+        document.getElementById('tries').innerHTML = tries;
+        console.log(hiddenWord);
+        document.getElementById('word').innerHTML = hiddenWord.join(' ');
+    }
+};
+
+function hiddenValue(arr, val,answer) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === val) {
+            return answer == val;
+        }
+    }
+    return false;
+}
+
+
+
+startGame();
+document.onkeyup = function (game) {
+
+    if (game.keyCode >= 65 && game.keyCode <= 90) {
+        var userInput = game.key;
+
+        for (var i = 0; i < word.length; i++) {
+            if (word[i] === userInput) {
+                hiddenWord[i] = userInput;
+            }
+            
+        }
+
+
+
+        // for (let i = 0; i < word.length; i++) {
+
+        //    if (word[i] === userInput) {
+        //         hiddenWord[i] = userInput;
+        //    }
+        //  }
+
+        guessedLetters.push(userInput);
+        document.getElementById('letters').innerHTML = guessedLetters;
+        document.getElementById('word').innerHTML = hiddenWord.join(' ');
+
+        console.log(userInput);
+    }
+    else {
+        alert('Please enter a letter.');
+
+    }
+    document.getElementById('guesses').innerHTML = tries;
+}
 
